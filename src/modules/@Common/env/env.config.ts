909083b0a8b8389f7ed75167256a482d91fd5ba6
JA21 <jorgeandres2104@gmail.com>
2021-11-30 20:19:59 -0500

    feat:(config archive env )

--- src/modules/@Common/env/env.config.ts
+++ src/modules/@Common/env/env.config.ts
@@ -5,9 +5,9 @@ export default registerAs('app', () => ({
   env: process.env.NODE_ENV,
   name: process.env.APP_NAME,
   host: process.env.APP_HOST,
-  prefix: process.env.APP_URL_PREFIX,
+  
   port: process.env.APP_PORT,
-  jwtKey: process.env.JWT_KEY,
+ 
   db_Type: process.env.DB_TYPE,
   db_Host: process.env.DB_HOST,
   db_Database: process.env.DB_DATABASE,
