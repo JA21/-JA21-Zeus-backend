a519b73dfb00936a9988387320a22e2a4cc7ed6a
JA21 <jorgeandres2104@gmail.com>
2022-01-08 00:06:22 -0500

    feat:(new controler and servide inventory)

--- src/modules/inventory/inventory.controller.ts
+++ src/modules/inventory/inventory.controller.ts
@@ -1,21 +1,58 @@
-import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post, Put, Res, NotFoundException, Delete, Query } from '@nestjs/common';
 
+import { InventoryService } from './inventory.service'
+import { CreateInvetoryDto } from './dto/inventory_dto.dto'
 
-import {CreateInvetoryDto} from './dto/inventory_dto.dto'
 @Controller('inventory')
 export class InventoryController {
 
-  @Get()
-  async AllInvetory() {
-    return "inventario";
+  constructor(private iventoryService: InventoryService) { }
+
+  @Get('/')
+  async AllInvetory(@Res() res) {
+    const respond = await this.iventoryService.getInventory();
+    return res.status(HttpStatus.OK).json({
+      respond
+    });
   }
 
   @Post('/create')
-  async createInventory(@Res() res, @Body() body:CreateInvetoryDto) {
-    console.log(body)
+  async createInventory(@Res() res, @Body() body: CreateInvetoryDto) {
+    const respond = await this.iventoryService.createInventory(body);
     return res.status(HttpStatus.OK).json({
-      message: 'recived'
+      message: 'Inventory succesfully created',
+      respond
     });
   }
+
+  @Get('/:id')
+  async getInventoryById(@Res() res, @Param('id') id: string) {
+    const respond = await this.iventoryService.getInventoryById(id);
+    if (!respond) throw new NotFoundException('Iventory does not exists')
+    return res.status(HttpStatus.OK).json(respond);
+  }
+
+
+  @Delete('/delete')//consulta
+  async deleteIventory(@Res() res, @Query('id') id: string) {
+    const respond = await this.iventoryService.deleteInventory(id);
+    if (!respond) throw new NotFoundException('Iventory does not exists')
+    return res.status(HttpStatus.OK).json({
+      message: 'Inventory delete succesfully',
+      respond
+    })
+  }
+
+
+  @Put('/update')
+  async updateIventory(@Res() res, @Body() body: CreateInvetoryDto, @Param('id') id: string) {
+    const respond = await this.iventoryService.updateInvetory(id, body);
+    return res.status(HttpStatus.OK).json({
+      message: 'Inventory succesfully update',
+      respond
+    })
+
+  }
+
 
 }
--- src/modules/inventory/inventory.service.ts
+++ src/modules/inventory/inventory.service.ts
@@ -3,24 +3,35 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 
 import { Iventory } from './interfaces/inventory.interface';
+import { CreateInvetoryDto } from './dto/inventory_dto.dto';
+
 @Injectable()
 export class InventoryService {
   constructor(@InjectModel('Inventory') private readonly inventoryModel: Model<Iventory>) { }
 
   async getInventory(): Promise<Iventory[]> {
-    const inventory =await this.inventoryModel.find();
+    const inventory = await this.inventoryModel.find();
     return inventory;
   }
 
-  createInventory() {
-
+  async getInventoryById(id: string): Promise<Iventory> {
+    const iventory = await this.inventoryModel.findById(id);
+    return iventory;
   }
 
-  updateInvetory() {
-
+  async createInventory(createInventoryDto: CreateInvetoryDto): Promise<Iventory> {
+    const iventory = new this.inventoryModel(createInventoryDto);
+    return await iventory.save();
   }
 
-  deleteInventory() {
+  async updateInvetory(id:string, createProductDto):Promise<Iventory> {
+    const iventory= await this.inventoryModel.findByIdAndUpdate(id,createProductDto,
+      {new:true});
+    return iventory;
+  }
 
+  async deleteInventory(id: string): Promise<Iventory> {
+    const inventory = await this.inventoryModel.findByIdAndDelete(id);
+    return inventory;
   }
 }
